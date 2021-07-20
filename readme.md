e95464cc518b2d942529df093b536277ff9acf69
Rob Fairclough <rob@silverchip.com>
2021-07-20 10:04:55 +0100

    lunch & learn prep

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Mark Baker, Lunch Request taker
+
+## What?
+
+This is the source code for a Twitter bot, built to generate suggestions for what to eat for lunch.
+
+## How?
+
+To run this locally
+
+1. Clone the repo down to your machine
+2. Run `npm install` in the repo to install dependencies
+3. ## Create a .env file with your Twitter Developer credentials. You'll want these:
+    - CONSUMER_KEY
+    - CONSUMER_SECRET
+    - ACCESS_TOKEN
+    - ACCESS_TOKEN_SECRET
+    - BEARER_TOKEN (maybe)
+4. Run `npm start` to run locally using ts-node.
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,59 @@
 require('dotenv').config();
 import TwitterApi from 'twitter-api-v2';
 
-// Set up client with dotenv file/env variables
-const twitter = new TwitterApi({
-    appKey: process.env.CONSUMER_KEY ?? '',
-    appSecret: process.env.CONSUMER_SECRET ?? '',
-    // Following access tokens are not required if you are
-    // at part 1 of user-auth process (ask for a request token)
-    // or if you want a app-only client (see below)
-    accessToken: process.env.ACCESS_TOKEN ?? '',
-    accessSecret: process.env.ACCESS_TOKEN_SECRET ?? '',
-});
-const client = twitter.readWrite;
-
 // Top level async function so we can await in script
 (async () => {
-    const me = await client.currentUser();
-    console.log({ me });
-    const user = await client.v2.userByUsername('robfairclough');
-    console.log({ user });
-    await client.v2.follow(me.id_str, user.data.id);
+    // ? Setup OAuth 1 client. Needed for general functions, logins, replies etc.
+    const twitter = new TwitterApi({
+        appKey: process.env.CONSUMER_KEY ?? '',
+        appSecret: process.env.CONSUMER_SECRET ?? '',
+        accessToken: process.env.ACCESS_TOKEN ?? '',
+        accessSecret: process.env.ACCESS_TOKEN_SECRET ?? '',
+    });
+    // ? OAuth 2 (app-only) - needed for streams and some v2 api functionality.
+    const appTwitter = await twitter.appLogin();
+    const me = await twitter.currentUser();
+
+    // ? Setting up a search stream, to listen for tweets directed at the bot.
+    const rules = await appTwitter.v2.streamRules();
+    const ruleValue = '@' + me.screen_name;
+    // Erase previous rules
+    const doOtherRulesExist = rules?.data.find(({ value }) => value !== ruleValue);
+    if (doOtherRulesExist) {
+        await appTwitter.v2.updateStreamRules({
+            delete: { ids: rules.data.map(rule => rule.id) },
+        });
+    }
+    // Add my search rule
+    const doesRuleExist = rules?.data?.find(({ value }) => value === ruleValue);
+    if (!doesRuleExist) {
+        await appTwitter.v2.updateStreamRules({
+            add: [{ value: '@' + me.screen_name }],
+        });
+    }
+
+    // ? Establish tweet stream
+    const stream = await appTwitter.v2.searchStream({
+        'tweet.fields': ['in_reply_to_user_id'],
+    });
+    // ? Handle tweets from stream
+    for await (const { data: tweet } of stream) {
+        console.log({ tweet });
+        // If the bot is not directly mentioned, ignore
+        if (tweet.in_reply_to_user_id !== me.id_str) continue;
+        // ? Like the tweet
+        await twitter.v2.like(me.id_str, tweet.id);
+        // todo generate lunch suggestions
+        const lunchSuggestion = '';
+        twitter.v1.reply(lunchSuggestion, tweet.id).catch((err: Error) => console.log(err.message));
+        // ? follow user that asked for lunch
+        if (!!tweet.author_id && tweet.author_id !== me.id_str) {
+            const following = await appTwitter.v2.following(me.id_str);
+            // don't follow if already following
+            if (following.data.find(dat => dat.id === tweet.author_id)) continue;
+            const user = await appTwitter.v2.user(tweet.author_id);
+            await appTwitter.v2.follow(me.id_str, user.data.id);
+        }
+    }
+    // todo daily tweet at lunchtime?
 })().catch(console.log);
